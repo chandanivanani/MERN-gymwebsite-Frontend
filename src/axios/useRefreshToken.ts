5c833b006e9e34c19b27789efdaaf31907836bcb
Chandani <chandanivanani@gmail.com>
2025-03-25 16:50:04 +0530

    update useRefreshToken

--- src/axios/useRefreshToken.ts
+++ src/axios/useRefreshToken.ts
@@ -1,18 +1,15 @@
-import axios from "./axios";
+import { axiosPrivate } from './axios';
 import {useNavigate} from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { logout,updateToken } from "../store/slices/authSlice";
 
 const useRefreshToken = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    // const { token } = useSelector((state:any) => state.auth);
 
     const refresh = async () => {
         try {
-            const response = await axios.get("auth/refresh",{
-                withCredentials:true,
-            });
+            const response = await axiosPrivate.get("auth/refresh");
 
             console.log("Refreshed token:",response.data);
             const newToken = response.data.data;
